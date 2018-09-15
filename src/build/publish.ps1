854da479f2555153c5d80ef948c494552c36e05a
Jasper H. Bojsen <liveowl@outlook.com>
2018-09-15 13:06:23 +0200

    v6.0

--- src/build/publish.ps1
+++ src/build/publish.ps1
@@ -1,12 +1,12 @@
 param([string]$betaver)
 
 if ([string]::IsNullOrEmpty($betaver)) {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interfaces\ISSDP.UPnP.Netstandard\bin\Release\netstandard2.0\ISSDP.UPnP.PCL.dll')).Version.ToString(3)
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISimpleHttpListener.Rx\bin\Release\netstandard2.0\ISimpleHttpListener.Rx.dll')).Version.ToString(3)
 	}
 else {
-	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interfaces\ISSDP.UPnP.Netstandard\bin\Release\netstandard2.0\ISSDP.UPnP.PCL.dll')).Version.ToString(3) + "-" + $betaver
+	$version = [Reflection.AssemblyName]::GetAssemblyName((resolve-path '..\interface\ISimpleHttpListener.Rx\bin\Release\netstandard2.0\ISimpleHttpListener.Rx.dll')).Version.ToString(3) + "-" + $betaver
 }
 
 .\build.ps1 $version
 
-Nuget.exe push ".\NuGet\SSDP.UPnP.PCL.$version.symbols.nupkg" -Source https://www.nuget.org
+Nuget.exe push ".\NuGet\SimpleHttpListener.Rx.$version.symbols.nupkg" -Source https://www.nuget.org
